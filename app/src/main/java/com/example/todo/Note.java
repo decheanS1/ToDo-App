56f6a8b56f8e1b3364787836c13b2cbcdaa427ca
DecheanSherpa <sdechean20@tbc.edu.np>
2023-05-09 19:00:59 +0545

    Worked on recyclerview for notes

---
+++ app/src/main/java/com/example/todo/Note.java
@@ -0,0 +1,31 @@
+package com.example.todo;
+
+public class Note {
+
+    private String title;
+    private String description;
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    public Note(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+}
